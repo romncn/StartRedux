c9111e4da5ec24dfd96f430b4b54ba7f66b76db2
romncn <romncn@gmail.com>
2020-04-04 23:58:47 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import App from './App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state += action.payload;
+      break;
+    case "SUBTRACT":
+      state -= action.payload;
+      break;
+    default:
+  }
+  return state;
+}
+const store = createStore(reducer, 15000);
+
+store.subscribe(()=>{
+  console.log("Update Store:",store.getState());
+})
+store.dispatch({
+  type: "ADD",
+  payload: 500
+// })
+// store.dispatch({
+//   type: "SUBTRACT",
+//   payload: 1000
+ })
